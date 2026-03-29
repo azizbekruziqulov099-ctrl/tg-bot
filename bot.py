9ec6b9f19a31ce068d2dd0cb95e19af1177329ed
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-03-30 00:30:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import os
+
 TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise ValueError("TOKEN topilmadi")
 
 menu = ReplyKeyboardMarkup(
     [
@@ -11,6 +15,13 @@ menu = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
+user_balances = {}
+
+def get_balance(user_id):
+    if user_id not in user_balances:
+        user_balances[user_id] = 10
+    return user_balances[user_id]
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Assalomu alaykum!\nBotga xush kelibsiz 🚀",
@@ -19,19 +30,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
+    user_id = update.message.from_user.id
 
     if text == "📊 PPT yaratish":
-        await update.message.reply_text("Mavzu yuboring 📩")
+        if get_balance(user_id) < 10:
+            await update.message.reply_text("❌ Tanga yetarli emas")
+            return
+        
+        user_balances[user_id] -= 10
+        await update.message.reply_text("⏳ PPT tayyorlanmoqda...")
 
     elif text == "🪙 Tangalarim":
-        await update.message.reply_text("Sizda hozircha 0 ta tanga mavjud 🪙")
+        await update.message.reply_text(f"{get_balance(user_id)} ta tanga bor")
 
     elif text == "ℹ️ Yordam":
-        await update.message.reply_text(
-            "Bu bot sizga prezentatsiya yaratib beradi.\n\n"
-            "📊 PPT yaratish - prezentatsiya\n"
-            "🪙 Tangalar - balans\n"
-        )
+        await update.message.reply_text("Bot PPT yaratadi")
 
     else:
         await update.message.reply_text("Tushunmadim 😅")
